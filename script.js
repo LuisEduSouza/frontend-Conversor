541a78568dbffdee75e0f8b25a10d2d397bb8a4f
LuisEduSouza <luisduduovaes@gmail.com>
2026-05-28 21:33:06 -0300

    Finalizando branch reserva sem logica de dois servidores

--- script.js
+++ script.js
@@ -2,32 +2,47 @@ document.querySelector("form").addEventListener("submit", async function (event)
     event.preventDefault();
 
     const botao = document.getElementById("btnConversao");
-
     botao.disabled = true;
 
     try {
         let entrada = document.getElementById("entrada").value;
         entrada = entrada.toUpperCase();
-        const base = Number(document.getElementById("base").value);
 
+        const baseOrigem = Number(document.getElementById("baseOrigem").value);
+        const baseFinal = Number(document.getElementById("baseFinal").value);
 
-        for (let i = 0; i <= entrada.length - 1; i++) {
+        // validação
+        for (let i = 0; i < entrada.length; i++) {
             const caractere = entrada[i];
-            if (base <= 10) {
-                if (caractere < '0' || caractere > '9' || Number(caractere) >= base) {
-                    alert("Entrada inválida! Não é permitido dígitos maiores ou iguais a base");
-                    throw new Error("Entrada inválida! A entrada possui dígitos maior ou igual a base");
+
+            if (baseOrigem <= 10) {
+                if (
+                    caractere < '0' ||
+                    caractere > '9' ||
+                    Number(caractere) >= baseOrigem
+                ) {
+                    alert("Entrada inválida!");
+                    throw new Error("Dígito inválido para essa base");
                 }
             }
-            if (base === 16) {
-                if ((caractere < '0' && caractere > '9') || (caractere < 'A' && caractere > 'F')) {
-                    alert("Entrada inválida! Não é permitido dígitos maiores ou iguais a base");
-                    throw new Error("Entrada inválida! A entrada possui dígitos maior ou igual a base");
+
+            if (baseOrigem === 16) {
+                const ehNumero = caractere >= '0' && caractere <= '9';
+                const ehHex = caractere >= 'A' && caractere <= 'F';
+
+                if (!ehNumero && !ehHex) {
+                    alert("Entrada inválida!");
+                    throw new Error("Caractere inválido para hexadecimal");
                 }
             }
         }
 
-        await enviarRequisicao();
+        await enviarRequisicao(
+            entrada,
+            baseOrigem,
+            baseFinal
+        );
+
     } catch (erro) {
         console.error(erro);
     } finally {
@@ -35,29 +50,36 @@ document.querySelector("form").addEventListener("submit", async function (event)
     }
 });
 
+async function enviarRequisicao(entrada, baseOrigem, baseFinal) {
 
-async function enviarRequisicao() {
+    // servidor TS
+    const url = "http://localhost:3333";
+
     try {
+
         const dadosConversao = {
-            entrada: document.getElementById("entrada").value,
-            baseOrigem: Number(document.getElementById("baseOrigem").value),
-            baseFinal: Number(document.getElementById("baseFinal").value)
+            entrada: entrada,
+            baseOrigem: baseOrigem,
+            baseFinal: baseFinal
         };
 
-        const resposta = await fetch('http://localhost:3333/converter', {
+        const resposta = await fetch(`${url}/converter`, {
             method: 'POST',
             headers: {
-                'Content-type': 'application/json'
+                'Content-Type': 'application/json'
             },
             body: JSON.stringify(dadosConversao)
-        })
+        });
 
         if (!resposta.ok) {
             throw new Error("Erro ao obter resposta do servidor");
         }
 
         const data = await resposta.json();
-        alert(data);
+
+        console.log(data);
+        alert(JSON.stringify(data));
+
     } catch (error) {
         console.log(error);
         alert("Não foi possível fazer a conversão");
